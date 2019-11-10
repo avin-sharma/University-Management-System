251db903f404c94d614c8c62639f69cbf6aadc85
Avin Sharma <sharmaavin@hotmail.com>
2019-11-10 17:50:47 -0500

    Moved code into a try excep block.

--- app.py
+++ app.py
@@ -2,5 +2,11 @@ import os
 from university import University
 
 if __name__ == "__main__":
-    stevens = University(os.path.dirname(os.path.abspath(__file__)), (';', True), ('|', True), ('|', True), ('\t', True))
-    print(stevens)
+    try:
+        stevens = University(os.path.dirname(os.path.abspath(__file__)), ('\t', True), ('\t', True), ('\t', True), ('\t', True))
+        print(stevens)
+
+    except (KeyError, ValueError) as e:
+        print("Bad data encountered.\n" + (e))
+    except FileNotFoundError as e:
+        print('Wrong folder path or missing files.\n' + str(e))
