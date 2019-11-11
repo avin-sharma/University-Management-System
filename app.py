302a1117e5c1f4a378344acc19f539265fd30d4d
Avin Sharma <sharmaavin@hotmail.com>
2019-11-10 19:32:30 -0500

    Refactored for better readability.

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from university import University
 
 if __name__ == "__main__":
     try:
-        stevens = University(os.path.dirname(os.path.abspath(__file__)), ('\t', True), ('\t', True), ('\t', True), ('\t', True))
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        stevens = University(current_dir, ('\t', True), ('\t', True), ('\t', True), ('\t', True), os.path.join(current_dir, 'database.db'))
         print(stevens)
-
     except (KeyError, ValueError) as e:
         print("Bad data encountered.\n" + (e))
     except FileNotFoundError as e:
